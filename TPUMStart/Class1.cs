b0b8248f7e9b6a7aa54bda1ed4be349ad6d4dd42
242566 Wlodarski Bartlomiej <242566@edu.p.lodz.pl>
2025-03-23 17:47:02 +0100

    [+] Added calculator operations [+] Added calculator unit tests

--- TPUMStart/Class1.cs
+++ TPUMStart/Class1.cs
@@ -1,7 +1,25 @@
 ﻿namespace TPUMStart
 {
-    public class Class1
+    public class Calculator
     {
+        public float Add(float x, float y)
+        {
+            return x + y;
+        }
 
+        public float Subtract(float x, float y)
+        {
+            return x - y;
+        }
+
+        public float Multiply(float x, float y)
+        {
+            return x * y;
+        }
+
+        public float Divide(float x, float y)
+        {
+            return x / y;
+        }
     }
 }
--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -1,14 +1,39 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+using TPUMStart;
 
 namespace UnitTests
 {
-    [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        [Fact]
+        public void TestCalculatorAdd()
         {
+            Calculator calculator = new Calculator();
+
+            Assert.Equal(2.75f, calculator.Add(1.5f, 1.25f));
+        }
+
+        [Fact]
+        public void TestCalculatorSubtract()
+        {
+            Calculator calculator = new Calculator();
+
+            Assert.Equal(0.25f, calculator.Subtract(1.5f, 1.25f));
+        }
+
+        [Fact]
+        public void TestCalculatorMultiply()
+        {
+            Calculator calculator = new Calculator();
+
+            Assert.Equal(1.875f, calculator.Multiply(1.5f, 1.25f));
+        }
+
+        [Fact]
+        public void TestCalculatorDivide()
+        {
+            Calculator calculator = new Calculator();
+
+            Assert.Equal(1.2f, calculator.Divide(1.5f, 1.25f));
         }
     }
 }
