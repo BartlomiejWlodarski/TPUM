09b888c3f9594abf09c1eca57b6e62958e64d62f
Mateusz Podgórski <bazyl0022@gmail.com>
2025-03-31 16:13:16 +0200

    Basic presentation layer setup added

---
+++ TPUMProject.Presentation/Model/ModelAbstractAPI.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TPUMProject.Logic.Abstract;
+
+namespace TPUMProject.Presentation.Model
+{
+    public abstract class ModelAbstractAPI
+    {
+        public static ModelAbstractAPI CreateModel(AbstractLogicAPI? logicAPI = default)
+        {
+            return new ModelLayer(logicAPI ?? AbstractLogicAPI.Create());
+        }
+
+        internal class ModelLayer : ModelAbstractAPI {
+            private readonly AbstractLogicAPI _logicLayer;
+            public ModelLayer(AbstractLogicAPI logicLayer)
+            {
+                _logicLayer = logicLayer;
+            }
+        }
+    }
+}
---
+++ TPUMProject.Presentation/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using TPUMProject.Presentation.Model;
+
+namespace TPUMProject.Presentation.ViewModel
+{
+    public class MainWindowViewModel : IDisposable, INotifyPropertyChanged
+    {
+        private ModelAbstractAPI ModelLayer;
+        private string _testString = "test1";
+
+        public string TestString
+        {
+            get { return _testString; }
+            set
+            {
+                _testString = value;
+                RaisePropertyChanged(nameof(_testString));
+            }
+        }
+
+        public MainWindowViewModel() : this(null)
+        { }
+
+        internal MainWindowViewModel(ModelAbstractAPI modelLayerAPI)
+        {
+            ModelLayer = modelLayerAPI == null ? ModelAbstractAPI.CreateModel() : modelLayerAPI;
+        }
+
+
+
+
+
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void RaisePropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
