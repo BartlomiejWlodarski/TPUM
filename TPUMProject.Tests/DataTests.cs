24d4ada9d8bd22640a112989a57650dbe7b80528
242566 Wlodarski Bartlomiej <242566@edu.p.lodz.pl>
2025-04-01 19:45:11 +0200

    [+] Added dependency injection to tests [*] Updated BuyBookTest

--- TPUMProject.Tests/DataTests.cs
+++ TPUMProject.Tests/DataTests.cs
@@ -5,13 +5,18 @@ namespace TPUMProject.Tests
     [TestClass]
     public class DataTests
     {
+        private readonly AbstractDataAPI _dataAPI;
+
+        public DataTests()
+        {
+            _dataAPI = AbstractDataAPI.Create("testUser", 10);
+        }
+
         [TestMethod]
         public void BookTest()
         {
-            AbstractDataAPI dataAPI = AbstractDataAPI.Create("test", 10);
-
-            IBook book = dataAPI.CreateBook("Pan Tadeusz", "Adam Mickiewicz", 20);
-            IBook book2 = dataAPI.CreateBook("Quo vadis", "Henryk Sienkiewicz", 25);
+            IBook book = _dataAPI.CreateBook("Pan Tadeusz", "Adam Mickiewicz", 20);
+            IBook book2 = _dataAPI.CreateBook("Quo vadis", "Henryk Sienkiewicz", 25);
 
             Assert.AreEqual("Pan Tadeusz", book.Title);
 
@@ -25,15 +30,13 @@ namespace TPUMProject.Tests
         [TestMethod]
         public void CreateBookTest()
         {
-            AbstractDataAPI dataAPI = AbstractDataAPI.Create("test", 10);
+            Assert.AreEqual(0, _dataAPI.CountBooks());
 
-            Assert.AreEqual(0, dataAPI.CountBooks());
+            IBook book = _dataAPI.CreateBook("Pan Tadeusz", "Adam Mickiewicz", 20);
 
-            IBook book = dataAPI.CreateBook("Pan Tadeusz", "Adam Mickiewicz", 20);
+            _dataAPI.AddBook(book);
 
-            dataAPI.AddBook(book);
-
-            Assert.AreEqual(1, dataAPI.CountBooks());
+            Assert.AreEqual(1, _dataAPI.CountBooks());
         }
     }
 }
--- TPUMProject.Tests/LogicTests.cs
+++ TPUMProject.Tests/LogicTests.cs
@@ -6,53 +6,55 @@ namespace TPUMProject.Tests
     [TestClass]
     public class LogicTests
     {
+        private readonly AbstractLogicAPI _logicAPI;
+
+        public LogicTests()
+        {
+            _logicAPI = AbstractLogicAPI.Create("testUser", 50);
+        }
+
         [TestMethod]
         public void AddNewBookTest()
         {
-            AbstractLogicAPI logicAPI = AbstractLogicAPI.Create("test", 10);
+            Assert.AreEqual(0, _logicAPI.BookService.GetAvailableBooks().Count());
 
-            Assert.AreEqual(0, logicAPI.BookService.GetAvailableBooks().Count());
+            _logicAPI.BookService.AddNewBook("Pan Tadeusz", "Adam Mickiewicz", 20);
 
-            logicAPI.BookService.AddNewBook("Pan Tadeusz", "Adam Mickiewicz", 20);
-
-            Assert.AreEqual(1, logicAPI.BookService.GetAvailableBooks().Count());
+            Assert.AreEqual(1, _logicAPI.BookService.GetAvailableBooks().Count());
         }
 
         [TestMethod]
         public void BuyBookTest()
         {
-            AbstractLogicAPI logicAPI = AbstractLogicAPI.Create("test", 50);
+            Assert.AreEqual(false, _logicAPI.BookService.BuyBook(0));
 
-            Assert.AreEqual(false, logicAPI.BookService.BuyBook(0));
+            _logicAPI.BookService.AddNewBook("Pan Tadeusz", "Adam Mickiewicz", 20);
+            _logicAPI.BookService.AddNewBook("Quo vadis", "Henryk Sienkiewicz", 100);
 
-            logicAPI.BookService.AddNewBook("Pan Tadeusz", "Adam Mickiewicz", 20);
+            Assert.AreEqual(false, _logicAPI.BookService.BuyBook(0));
 
-            Assert.AreEqual(false, logicAPI.BookService.BuyBook(0));
+            Assert.AreEqual(false, _logicAPI.BookService.BuyBook(2));
 
-            Assert.AreEqual(true, logicAPI.BookService.BuyBook(1));
+            Assert.AreEqual(true, _logicAPI.BookService.BuyBook(1));
 
-            Assert.AreEqual(30, logicAPI.User.Balance);
+            Assert.AreEqual(30, _logicAPI.User.Balance);
 
-            Assert.AreEqual(0, logicAPI.BookService.GetAvailableBooks().Count());
+            Assert.AreEqual(1, _logicAPI.BookService.GetAvailableBooks().Count());
 
-            Assert.AreEqual(false, logicAPI.BookService.BuyBook(1));
-
-            // TODO: more buy tests
+            Assert.AreEqual(false, _logicAPI.BookService.BuyBook(1));
         }
 
         [TestMethod]
         public void RecommendedBookTest()
         {
-            AbstractLogicAPI logicAPI = AbstractLogicAPI.Create("test", 10);
+            _logicAPI.BookService.AddNewBook("Pan Tadeusz", "Adam Mickiewicz", 20);
+            _logicAPI.BookService.AddNewBook("Quo vadis", "Henryk Sienkiewicz", 25);
 
-            logicAPI.BookService.AddNewBook("Pan Tadeusz", "Adam Mickiewicz", 20);
-            logicAPI.BookService.AddNewBook("Quo vadis", "Henryk Sienkiewicz", 25);
+            Assert.AreEqual(_logicAPI.BookService.GetAvailableBooks().ToList()[0].Recommended, _logicAPI.BookService.GetAvailableBooks().ToList()[1].Recommended);
 
-            Assert.AreEqual(logicAPI.BookService.GetAvailableBooks().ToList()[0].Recommended, logicAPI.BookService.GetAvailableBooks().ToList()[1].Recommended);
+            _logicAPI.BookService.GetRandomRecommendedBook();
 
-            logicAPI.BookService.GetRandomRecommendedBook();
-
-            Assert.AreNotEqual(logicAPI.BookService.GetAvailableBooks().ToList()[0].Recommended, logicAPI.BookService.GetAvailableBooks().ToList()[1].Recommended);
+            Assert.AreNotEqual(_logicAPI.BookService.GetAvailableBooks().ToList()[0].Recommended, _logicAPI.BookService.GetAvailableBooks().ToList()[1].Recommended);
         }
     }
 }
