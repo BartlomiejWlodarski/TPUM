8d6a1eaea163c5f2a23ea15d213405409964a2ef
242500 <126494791+bazyl02@users.noreply.github.com>
2025-04-01 15:02:36 +0200

    Added ModelBook class to display book information in View Layer

---
+++ TPUMProject.Presentation/Model/ModelBook.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using TPUMProject.Data.Abstract;
+
+namespace TPUMProject.Presentation.Model
+{
+    public enum ModelGenre
+    {
+        Uncategorized,
+        Action,
+        Drama,
+        Romance,
+        Mystery,
+        Science_Fiction
+    }
+    public class ModelBook : INotifyPropertyChanged
+    {
+        
+
+        int Id { get; set; }
+        string Title { get; set; }
+        string Author { get; set; }
+        ModelGenre Genre { get; set; }
+        decimal Price { get; set; }
+        bool Recommended { get; set; }
+
+        public ModelBook(IBook book)
+        {
+            Id = book.Id;
+            Title = book.Title;
+            Author = book.Author;
+            Genre = (ModelGenre)book.Genre;
+            Price = book.Price;
+            Recommended = book.Recommended;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
