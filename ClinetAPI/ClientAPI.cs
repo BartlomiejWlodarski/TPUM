0cd6f54eb1c0e64d5f0702de60f7b10a8399839c
Mateusz Podgórski <bazyl0022@gmail.com>
2025-04-07 14:35:03 +0200

    Progress on Server Presentation

--- ClinetAPI/ClientAPI.cs
+++ ClinetAPI/ClientAPI.cs
@@ -14,6 +14,41 @@ namespace ClinetAPI
     }
 
     [Serializable]
+    public class GetBooksCommand : ServerCommand
+    {
+        public static string StaticHeader = "GetBooks";
+
+        public GetBooksCommand() : base(StaticHeader) { }
+    }
+
+    [Serializable]
+    public class SellBookCommand : ServerCommand
+    {
+        public static string StaticHeader = "SellBook";
+
+        public int BookID;
+        public string Username;
+
+        public SellBookCommand(int id, string username) : base(StaticHeader) 
+        {
+            BookID = id;
+            Username = username;
+        }
+    }
+
+    [Serializable]
+    public class GetUserCommand : ServerCommand
+    {
+        public static string StaticHeader = "GetUser";
+
+        string Username;
+
+        public GetUserCommand(string username) : base(StaticHeader)
+        {
+            Username = username;
+        }
+    }
+    [Serializable]
     public struct BookDTO
     {
         public int Id;
@@ -33,4 +68,80 @@ namespace ClinetAPI
             this.Genre = Genre;
         }
     }
+
+    [Serializable]
+    public struct UserDTO
+    {
+        public string Username;
+        public decimal Balance;
+        public BookDTO[] Books;
+
+        public UserDTO(string username, decimal balance, BookDTO[] booksId)
+        {
+            this.Username = username;
+            this.Balance = balance;
+            this.Books = booksId;
+        }
+    }
+
+    [Serializable]
+    public abstract class ServerResponse
+    {
+        public string Header { get; private set; }
+
+        protected ServerResponse(string header)
+        {
+            Header = header;
+        }
+    }
+    [Serializable]
+    public class AllBooksUpdateResponse : ServerResponse
+    {
+        public static readonly string StaticHeader = "AllBooksUpdate";
+
+        public BookDTO[]? Books;
+
+        public AllBooksUpdateResponse() : base(StaticHeader) { }
+    }
+
+    [Serializable]
+    public class BookChangedResponse : ServerResponse
+    {
+        public static readonly string StaticHeader = "BookChanged";
+
+        public BookDTO book;
+        public int changeType;
+
+        public BookChangedResponse(int change) : base(StaticHeader) { changeType = change; }
+    }
+
+
+    [Serializable]
+    public class TransactionResultResponse : ServerResponse
+    {
+        public static readonly string StaticHeader = "TransactactionResult";
+
+        public int BookID;
+        public string Username;
+        public int ResultCode; 
+        // 0 - success 1 - not enought money 2 - book not found 3 - unknown error
+
+        public TransactionResultResponse(int bookId, string username, int resultCode) : base(StaticHeader) 
+        {  
+            BookID = bookId;
+            Username = username;
+            ResultCode = resultCode;
+        }
+    }
+
+    [Serializable]
+    public class UserChangedResponse : ServerResponse
+    {
+        public static readonly string StaticHeader = "UserChanged";
+
+        public UserDTO User;
+
+        public UserChangedResponse() : base(StaticHeader) { }
+    }
+
 }
---
+++ ServerPresentation/ServerPresentation.cs
@@ -0,0 +1,123 @@
+﻿using ClinetAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TPUMProject.Logic.Abstract;
+
+namespace ServerPresentation
+{
+    internal class ServerPresentation
+    {
+        private readonly AbstractLogicAPI logicAPI;
+        private WebSocketConnection? connection;
+
+        private ServerPresentation(AbstractLogicAPI logicAPI)
+        {
+            this.logicAPI = logicAPI;
+            this.logicAPI.BookService.BookRepositoryChanged += HandleBookRepositoryChanged;
+            this.logicAPI.BookService.UserChanged += HandleUserChanged;
+        }
+
+        private async Task StartConnection()
+        {
+            while (true)
+            {
+                Console.WriteLine("Waiting for connection...");
+                await ServerWebSocket.StartServer(42069, OnConnect);
+            }
+        }
+
+        private void OnConnect(WebSocketConnection connection)
+        {
+            Console.WriteLine("Connected to " + connection);
+
+            connection.OnMessage = OnMessage;
+            connection.OnClose = OnClose;
+            connection.OnError = OnError;
+
+            this.connection = connection;
+        }
+
+        private async void OnMessage(string message)
+        {
+            if(connection == null) return;
+
+            Console.WriteLine("Message received: " + message);
+
+            Serializer serializer = Serializer.Create();
+
+            if(serializer.GetCommandHeader(message) == SellBookCommand.StaticHeader)
+            {
+                SellBookCommand sellBook = serializer.Deserialize<SellBookCommand>(message);
+                Task taks = Task.Run(async () => await SendBooks());
+            } 
+            else if(serializer.GetCommandHeader(message) == GetBooksCommand.StaticHeader)
+            {
+
+            }
+        }
+
+        private async Task SendBooks()
+        {
+            if(connection == null) return;
+
+            Console.WriteLine("Sending books' data...");
+
+            AllBooksUpdateResponse allBooksUpdateResponse = new AllBooksUpdateResponse();
+            IEnumerable<ILogicBook> books = logicAPI.BookService.GetAvailableBooks();
+            allBooksUpdateResponse.Books = books.Select(x => x.ConvertToDTO()).ToArray();
+
+            Serializer serializer = Serializer.Create();
+            string json = serializer.Serialize(allBooksUpdateResponse);
+            Console.WriteLine(json);
+
+            await connection.SendAsync(json);
+        }
+
+        private async void HandleBookRepositoryChanged(object sender, LogicBookRepositoryChangedEventArgs e)
+        {
+            if(connection == null) return;
+
+            Console.WriteLine("Changed book n.o: " + e.AffectedBook.Id);
+
+            BookChangedResponse bookChangedResponse = new BookChangedResponse(e.ChangedEventType);
+            bookChangedResponse.book = e.AffectedBook.ConvertToDTO();
+            
+            Serializer serializer = Serializer.Create();
+            string json = serializer.Serialize(bookChangedResponse);
+            Console.WriteLine(json);
+
+            await connection.SendAsync(json);
+        }
+
+        private async void HandleUserChanged(object sender, LogicUserChangedEventArgs e)
+        {
+            if(connection == null) return;
+
+            Console.WriteLine("User changed: " + e.user.Name);
+
+            UserChangedResponse userChangedResponse = new UserChangedResponse();
+            userChangedResponse.User = e.user.ConvertToDTO();
+
+            Serializer serializer = Serializer.Create();
+            string json = serializer.Serialize(userChangedResponse);
+            Console.WriteLine(json);
+
+            await connection.SendAsync(json);
+        }
+
+        private void OnClose()
+        {
+            Console.WriteLine("Connection closed");
+            connection = null;
+        }
+
+        private void OnError()
+        {
+            Console.WriteLine("Error!");
+        }
+
+    }
+}
