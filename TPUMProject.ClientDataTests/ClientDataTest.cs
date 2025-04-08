829807bfd73aece4eea434b9b83990298bf28014
242566 Wlodarski Bartlomiej <242566@edu.p.lodz.pl>
2025-04-08 16:47:41 +0200

    [+] Separated test projects

---
+++ TPUMProject.ClientDataTests/ClientDataTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPUMProject.ClientDataTests
+{
+    [TestClass]
+    public class ClientDataTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ TPUMProject.ClientLogicTests/ClientLogicTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPUMProject.ClientLogicTests
+{
+    [TestClass]
+    public class ClientLogicTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
