3c2f5aed1fe3c207cb148f607fdefd7a6d3aca92
242566 Wlodarski Bartlomiej <242566@edu.p.lodz.pl>
2025-04-09 20:48:28 +0200

    [+] Added ClientLogicTests [+] Added FakeClientData

--- TPUMProject.ClientLogicTests/ClientLogicTest.cs
+++ TPUMProject.ClientLogicTests/ClientLogicTest.cs
@@ -1,17 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ClientLogic.Abstract;
 
 namespace TPUMProject.ClientLogicTests
 {
     [TestClass]
     public class ClientLogicTest
     {
+        private AbstractLogicAPI logicAPI = AbstractLogicAPI.Create(new FakeDataAPI());
+
         [TestMethod]
-        public void TestMethod1()
+        public void GetBooksTest()
         {
+            Assert.AreEqual(2, logicAPI.GetBookService().GetAllBooks().Count());
+        }
+
+        [TestMethod]
+        public void BuyBookTest()
+        {
+            logicAPI.GetBookService().SellBook(0);
+
+            Assert.AreEqual(2, logicAPI.GetBookService().GetAllBooks().Count());
         }
     }
 }
---
+++ TPUMProject.ClientLogicTests/FakeClientData.cs
@@ -0,0 +1,294 @@
+﻿using ClientData.Abstract;
+using ClinetAPI;
+using System.Net.WebSockets;
+
+namespace TPUMProject.ClientLogicTests
+{
+    internal class FakeDataAPI : AbstractDataAPI
+    {
+        private readonly IUserContainer _userContainer;
+
+        private readonly IBookRepository _bookRepository;
+
+        private readonly IConnectionService _connectionService;
+        public FakeDataAPI()
+        {
+            _connectionService = new FakeConnectionService();
+            _bookRepository = new FakeBookRepository();
+            _userContainer = new FakeUserContainer();
+        }
+
+        public override IConnectionService GetConnectionService()
+        {
+            return _connectionService;
+        }
+
+        public override IBookRepository GetBookRepository()
+        {
+            return _bookRepository;
+        }
+
+        public override IUserContainer GetUserContainer()
+        {
+            return _userContainer;
+        }
+        private void OnMessage(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void GetUser(string username)
+        {
+            if (_connectionService.IsConnected())
+            {
+                Task task = Task.Run(async () => await RequestGetUser(username));
+            }
+        }
+
+        private Task RequestGetUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class FakeBookRepository : IBookRepository
+    {
+        private List<IBook> books = new();
+        private object bookLock = new object();
+
+        private HashSet<IObserver<BookRepositoryChangedEventArgs>> observers;
+
+        public event Action? AllBooksUpdated;
+        public event EventHandler<BookRepositoryChangedEventArgs>? BookRepositoryChangedHandler;
+
+        IConnectionService connectionService;
+
+        public FakeBookRepository()
+        {
+            books = new List<IBook>
+            {
+                new FakeBook("Adam Mickiewicz", "Pan Tadeusz", 20, false),
+                new FakeBook("Henryk Sienkiewicz", "Quo vadis", 40, false)
+            };
+        }
+
+        ~FakeBookRepository()
+        {
+            List<IObserver<BookRepositoryChangedEventArgs>> cachedObservers = observers.ToList();
+            foreach (IObserver<BookRepositoryChangedEventArgs>? observer in cachedObservers)
+            {
+                observer?.OnCompleted();
+            }
+        }
+
+        private void OnMessage(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RequestBooks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SellBook(int id, string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RequestUpdate()
+        {
+            if (connectionService.IsConnected())
+            {
+                Task task = Task.Run(async () => await RequestBooks());
+            }
+        }
+
+
+        public List<IBook> GetAllBooks()
+        {
+            return books;
+        }
+
+        public void ReplaceBook(IBook replacement)
+        {
+            lock (bookLock)
+            {
+                int index = books.FindIndex(book => book.Id == replacement.Id);
+                if (index > -1 && index < books.Count)
+                {
+                    books[index] = replacement;
+                }
+            }
+
+            foreach (IObserver<BookRepositoryChangedEventArgs>? observer in observers)
+            {
+                observer.OnNext(new BookRepositoryChangedEventArgs(replacement, BookRepositoryChangedEventType.Modified));
+            }
+        }
+
+        public void AddBook(IBook book)
+        {
+            lock (bookLock)
+            {
+                books.Add(book);
+            }
+        }
+
+        public void RemoveBook(int bookID)
+        {
+            lock (bookLock)
+            {
+                books.RemoveAll(x => x.Id == bookID);
+            }
+        }
+
+        public IBook? GetBookByID(int bookID)
+        {
+            IBook? result = null;
+            lock (bookLock)
+            {
+                result = books.Find(x => x.Id == bookID);
+            }
+            return result;
+        }
+
+        private void UpdateAllBooks(AllBooksUpdateResponse response)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDisposable Subscribe(IObserver<BookRepositoryChangedEventArgs> observer)
+        {
+            return new BookRepositoryDisposable(this, observer);
+        }
+
+        private void UnSubscribe(IObserver<BookRepositoryChangedEventArgs> observer)
+        {
+            observers.Remove(observer);
+        }
+
+        private class BookRepositoryDisposable : IDisposable
+        {
+            private readonly FakeBookRepository _bookRepository;
+            private readonly IObserver<BookRepositoryChangedEventArgs> _observer;
+
+            public BookRepositoryDisposable(FakeBookRepository bookRepository, IObserver<BookRepositoryChangedEventArgs> observer)
+            {
+                _bookRepository = bookRepository;
+                _observer = observer;
+            }
+            public void Dispose()
+            {
+                _bookRepository.UnSubscribe(_observer);
+            }
+        }
+    }
+
+    internal class FakeBook : IBook, ICloneable
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+        public bool Recommended { get; set; } = false;
+        public Genre Genre { get; set; }
+
+        public FakeBook(string author, string title, decimal price, bool recommended)
+        {
+            Author = author;
+            Title = title;
+            Price = price;
+            Recommended = recommended;
+        }
+        
+        public object Clone()
+        {
+            FakeBook clone = (FakeBook)MemberwiseClone();
+            clone.Title = string.Copy(Title);
+            clone.Author = string.Copy(Author);
+            return clone;
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Author} ({Price:C})";
+        }
+    }
+
+    internal class FakeConnectionService : IConnectionService
+    {
+        public event Action<string>? Logger;
+        public event Action? OnConnectStateChanged;
+        public event Action<string>? OnMessage;
+        public event Action? OnError;
+        public event Action? OnDisconnect;
+
+        internal WebSocketConnection? WebSocketConnection { get; private set; }
+        public Task Connect(Uri peerUri)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Disconnect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsConnected()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendAsync(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class FakeUserContainer : IUserContainer
+    {
+        public IUser user { get; private set; }
+
+        public event EventHandler<UserChangedEventArgs>? UserChanged;
+
+        public FakeUserContainer()
+        {
+            user = new FakeUser("Marcin", 30);
+        }
+
+        public void ChangeUser(IUser user)
+        {
+            this.user = user;
+            UserChanged?.Invoke(this, new UserChangedEventArgs(user));
+        }
+    }
+
+    internal class FakeUser : IUser
+    {
+        public string Name { get; }
+        public decimal Balance { get; set; }
+        private List<IBook> _purchasedBooks;
+        public FakeUser(string name, decimal initialBalance)
+        {
+            Name = name;
+            Balance = initialBalance;
+            _purchasedBooks = new List<IBook>();
+        }
+
+        public IEnumerable<IBook> PurchasedBooks => _purchasedBooks.AsReadOnly();
+
+        public void SetPurchasedBooks(IEnumerable<IBook> purchasedBooks)
+        {
+            _purchasedBooks = purchasedBooks.ToList();
+        }
+
+        public void AddPurchasedBook(IBook book)
+        {
+            if (book != null)
+            {
+                _purchasedBooks.Add(book);
+            }
+        }
+    }
+}
